da59c7b3be13eccf78b86e26e9d5d41842832fbe
Thomas Garcia <thomasjgarcia1@gmail.com>
2015-02-27 11:09:08 -0800

    tickets are now automatically created with a consultant and a manger.

--- tickets/create.php
+++ tickets/create.php
@@ -5,12 +5,14 @@
     $config = parse_ini_file("../server.conf");
     $db = new PDO("mysql:host={$config['host']};dbname={$config['database']};charset=utf8", "{$config['user']}", "{$config['password']}");
     $sql = $db->prepare("INSERT INTO tickets
-            (user, title, description, status)
-            values (:user, :title, :description, 5)");
+            (user, title, consultant, manager, description, status)
+            values (:user, :title, :consultant, :manager, :description, 5)");
             #5 is the status code for In Progress (should find a more readable way to do this)
     $sql->bindParam(':user', $_SESSION['id']);
     $sql->bindParam(':title', $_POST['title']);
+    $sql->bindValue(':consultant', 5);
+    $sql->bindValue(':manager', 5);
     $sql->bindParam(':description', $_POST['description']);
     $sql->execute();
-    header('Location: http://localhost/helpdesk/tickets/list.php', TRUE, 302);
+    header('Location: http://10.55.255.252/master/tickets/list.php', TRUE, 302);
 ?>
