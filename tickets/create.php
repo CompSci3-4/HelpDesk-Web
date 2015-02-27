19a63bb35e5a834c2a956b463b293b5859d3ff64
Thomas Garcia <thomasjgarcia1@gmail.com>
2015-02-26 16:33:21 -0800

    Added prepared SQL queries to tickets/create.php. This defends against SQL injection, and allows descriptions to contain quotation marks.

--- tickets/create.php
+++ tickets/create.php
@@ -4,10 +4,13 @@
         header('Location: http://localhost/helpdesk/login.php', TRUE, 302);
     $config = parse_ini_file("../server.conf");
     $db = new PDO("mysql:host={$config['host']};dbname={$config['database']};charset=utf8", "{$config['user']}", "{$config['password']}");
-    $sql = "INSERT INTO tickets
+    $sql = $db->prepare("INSERT INTO tickets
             (user, title, description, status)
-            values ({$_SESSION['id']}, '{$_POST['title']}', '{$_POST['description']}', 5)";
+            values (:user, :title, :description, 5)");
             #5 is the status code for In Progress (should find a more readable way to do this)
-    $db->query($sql);
+    $sql->bindParam(':user', $_SESSION['id']);
+    $sql->bindParam(':title', $_POST['title']);
+    $sql->bindParam(':description', $_POST['description']);
+    $sql->execute();
     header('Location: http://localhost/helpdesk/tickets/list.php', TRUE, 302);
 ?>
