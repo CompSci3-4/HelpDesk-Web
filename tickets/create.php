56e3280e8a42797431d003ff2a3cf5be533b07be
Thomas Garcia <thomasjgarcia1@gmail.com>
2015-05-14 10:57:16 -0700

    Fixed ticket creation

--- tickets/create.php
+++ tickets/create.php
@@ -1,14 +1,16 @@
 <?php
     require_once("../start_session.php");
+    require_once("../database/user.php");
     $sql = $db->prepare("INSERT INTO tickets
             (user, title, consultant, manager, description, status)
             values (:user, :title, :consultant, :manager, :description, 5)");
             #5 is the status code for In Progress (should find a more readable way to do this)
-    $sql->bindParam(':user', $_SESSION['id']);
-    $sql->bindParam(':title', $_POST['title']);
+    $user = new User($_SESSION['id']);
+    $sql->bindValue(':user', $user->getID());
+    $sql->bindValue(':title', $_POST['title']);
     $sql->bindValue(':consultant', 7);
     $sql->bindValue(':manager', 8);
-    $sql->bindParam(':description', $_POST['description']);
+    $sql->bindValue(':description', $_POST['description']);
     $sql->execute();
     header('Location: ' . $config['root_directory'] . '/tickets/list.php', TRUE, 302);
 ?>
