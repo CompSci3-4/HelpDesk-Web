42f1c7b50fac4d828a9c9f08abf668ffd2141e21
Thomas Garcia <thomasjgarcia1@gmail.com>
2015-05-21 10:45:48 -0700

    Updated database

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.15  Distrib 10.0.17-MariaDB, for Linux (x86_64)
+-- MySQL dump 10.15  Distrib 10.0.18-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: helpdesk_testing
 -- ------------------------------------------------------
--- Server version	10.0.17-MariaDB-log
+-- Server version	10.0.18-MariaDB-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,12 +25,14 @@ DROP TABLE IF EXISTS `messages`;
 CREATE TABLE `messages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ticket` int(11) DEFAULT NULL,
-  `content` text NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `sender` varchar(25) DEFAULT NULL,
+  `title` varchar(25) DEFAULT NULL,
+  `body` text,
   PRIMARY KEY (`id`),
   KEY `ticket` (`ticket`),
   CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`ticket`) REFERENCES `tickets` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +41,7 @@ CREATE TABLE `messages` (
 
 LOCK TABLES `messages` WRITE;
 /*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+INSERT INTO `messages` VALUES (1,5,'2015-05-21 16:44:50','tgarcia','test','does it work?'),(2,5,'2015-05-21 16:45:15','tgarcia','test','does it work?');
 /*!40000 ALTER TABLE `messages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -115,7 +118,7 @@ CREATE TABLE `tickets` (
   CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`consultant`) REFERENCES `users` (`id`),
   CONSTRAINT `tickets_ibfk_3` FOREIGN KEY (`manager`) REFERENCES `users` (`id`),
   CONSTRAINT `tickets_ibfk_4` FOREIGN KEY (`status`) REFERENCES `statuses` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -124,7 +127,7 @@ CREATE TABLE `tickets` (
 
 LOCK TABLES `tickets` WRITE;
 /*!40000 ALTER TABLE `tickets` DISABLE KEYS */;
-INSERT INTO `tickets` VALUES (2,5,5,5,'computer exploded',5,'0000-00-00 00:00:00','help me pls'),(3,6,5,5,'CAPS LOCK IS STUCK',5,'0000-00-00 00:00:00','help me pls'),(5,5,NULL,NULL,'test',5,'2015-01-11 07:41:42','help me pls'),(6,5,7,5,'my printer is broken',5,'2015-01-11 07:43:55','help me pls'),(7,5,7,5,'It exploded',5,'2015-02-20 16:37:50','printer problems'),(8,5,7,NULL,'test',5,'2015-02-20 16:40:01','testing'),(9,5,5,8,'doesnt work',6,'2015-02-20 16:40:35','promethean board'),(10,5,7,8,'please fix my problem.',5,'2015-02-26 23:39:52','php will not install'),(11,5,7,8,'I don\'t have any real problems.',5,'2015-02-27 00:29:22','Testing'),(12,5,5,8,'test',5,'2015-03-03 07:10:14','Testing'),(13,5,5,8,'test',5,'2015-03-03 07:10:17','Testing'),(14,5,7,8,'test',6,'2015-03-03 07:11:43','Testing'),(15,6,5,5,'test',5,'2015-03-15 01:02:06','test'),(16,6,5,5,'fsdfsdfs',5,'2015-03-15 04:29:59','Testing'),(17,6,5,5,'',5,'2015-03-15 04:30:12','sfsdf'),(18,6,5,5,'',5,'2015-03-15 04:30:15','d'),(19,6,5,5,'',5,'2015-03-15 04:30:17',''),(20,6,5,5,'',5,'2015-03-15 04:30:19',''),(21,6,5,5,'',5,'2015-03-15 04:30:21','sdfdsfsf'),(22,6,5,5,'dfsf',5,'2015-03-15 04:30:54','sdfsdf'),(23,6,5,5,'',5,'2015-03-15 04:31:37','d'),(24,6,5,5,'',5,'2015-03-15 04:32:21','d'),(25,6,5,5,'',5,'2015-03-15 04:32:40','sds'),(26,6,5,5,'d',5,'2015-03-15 05:10:48','Testing'),(27,5,7,8,'first ticket made over API',5,'2015-05-05 18:01:11','hello world!'),(28,5,7,8,'first ticket made over API',5,'2015-05-05 18:01:46','hello world!'),(29,5,7,8,'first ticket made over API',5,'2015-05-05 18:01:49','hello world!'),(30,5,7,8,'first ticket made over API',5,'2015-05-05 18:05:10','hello world!'),(31,5,7,8,'first ticket made over API',5,'2015-05-05 18:06:50','hello world!'),(32,5,7,8,'first ticket made over API',5,'2015-05-05 18:07:52','hello world!'),(33,5,7,8,'first ticket made over API',5,'2015-05-05 18:08:44','hello world!'),(34,5,7,8,'first ticket made over API',5,'2015-05-05 18:09:06','hello world!'),(35,5,7,8,'first ticket made over API',5,'2015-05-05 18:09:14','hello world!'),(36,5,7,8,'first ticket made over API',5,'2015-05-05 18:10:48','hello world!'),(37,5,7,8,'testing',5,'2015-05-06 05:33:31','hello'),(38,5,7,8,'testing',5,'2015-05-06 05:33:32','hello'),(39,5,7,8,'testing',5,'2015-05-06 05:33:33','hello');
+INSERT INTO `tickets` VALUES (2,5,5,5,'computer exploded',5,'0000-00-00 00:00:00','help me pls'),(3,6,5,5,'CAPS LOCK IS STUCK',5,'0000-00-00 00:00:00','help me pls'),(5,5,NULL,NULL,'test',5,'2015-01-11 07:41:42','help me pls'),(6,5,7,5,'my printer is broken',5,'2015-01-11 07:43:55','help me pls'),(7,5,7,5,'It exploded',5,'2015-02-20 16:37:50','printer problems'),(8,5,7,NULL,'test',5,'2015-02-20 16:40:01','testing'),(9,5,5,8,'doesnt work',6,'2015-02-20 16:40:35','promethean board'),(10,5,7,8,'please fix my problem.',5,'2015-02-26 23:39:52','php will not install'),(11,5,7,8,'I don\'t have any real problems.',5,'2015-02-27 00:29:22','Testing'),(12,5,5,8,'test',5,'2015-03-03 07:10:14','Testing'),(13,5,5,8,'test',5,'2015-03-03 07:10:17','Testing'),(14,5,7,8,'test',6,'2015-03-03 07:11:43','Testing'),(15,6,5,5,'test',5,'2015-03-15 01:02:06','test'),(16,6,5,5,'fsdfsdfs',5,'2015-03-15 04:29:59','Testing'),(17,6,5,5,'',5,'2015-03-15 04:30:12','sfsdf'),(18,6,5,5,'',5,'2015-03-15 04:30:15','d'),(19,6,5,5,'',5,'2015-03-15 04:30:17',''),(20,6,5,5,'',5,'2015-03-15 04:30:19',''),(21,6,5,5,'',5,'2015-03-15 04:30:21','sdfdsfsf'),(22,6,5,5,'dfsf',5,'2015-03-15 04:30:54','sdfsdf'),(23,6,5,5,'',5,'2015-03-15 04:31:37','d'),(24,6,5,5,'',5,'2015-03-15 04:32:21','d'),(25,6,5,5,'',5,'2015-03-15 04:32:40','sds'),(26,6,5,5,'d',5,'2015-03-15 05:10:48','Testing'),(27,5,7,8,'first ticket made over API',5,'2015-05-05 18:01:11','hello world!'),(28,5,7,8,'first ticket made over API',5,'2015-05-05 18:01:46','hello world!'),(29,5,7,8,'first ticket made over API',5,'2015-05-05 18:01:49','hello world!'),(30,5,7,8,'first ticket made over API',5,'2015-05-05 18:05:10','hello world!'),(31,5,7,8,'first ticket made over API',5,'2015-05-05 18:06:50','hello world!'),(32,5,7,8,'first ticket made over API',5,'2015-05-05 18:07:52','hello world!'),(33,5,7,8,'first ticket made over API',5,'2015-05-05 18:08:44','hello world!'),(34,5,7,8,'first ticket made over API',5,'2015-05-05 18:09:06','hello world!'),(35,5,7,8,'first ticket made over API',5,'2015-05-05 18:09:14','hello world!'),(36,5,7,8,'first ticket made over API',5,'2015-05-05 18:10:48','hello world!'),(37,5,7,8,'testing',5,'2015-05-06 05:33:31','hello'),(38,5,7,8,'testing',5,'2015-05-06 05:33:32','hello'),(39,5,7,8,'testing',6,'2015-05-06 05:33:33','hello'),(40,5,7,8,'test',5,'2015-05-21 01:55:33','test');
 /*!40000 ALTER TABLE `tickets` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -147,7 +150,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   KEY `position` (`position`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`position`) REFERENCES `positions` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -156,7 +159,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (5,'tom','garcia',4,'',0,'$2y$07$DARJKF7Ep6B0J4mHxCHlsOyWl7olbbbtINs2Fm3zvmCHM6TcI3l3m','tgarcia'),(6,'john','doe',1,'johndoe@example.com',101,'$2y$07$89eTTt8d7VKd3TCnvBLt4utX3CESra7Bv4PapTFgd7Oak0vzLcxD6','jdoe'),(7,'Consultant','Charles',2,'charles@example.com',200,'$2y$07$5qf7DNJJhd7f3oBHyG8dweSLjoKybHMP3/RVjdODCO7J.KTIeAkIe','charles'),(8,'Manager','Moe',3,'charles@example.com',200,'$2y$07$JZhXg8EDNA8Q7PkA3toMmuDnOt61q62ultOOq2gUZ8DrR.aK3pG7y','moe');
+INSERT INTO `users` VALUES (5,'tom','garcia',4,'',0,'$2y$07$DARJKF7Ep6B0J4mHxCHlsOyWl7olbbbtINs2Fm3zvmCHM6TcI3l3m','tgarcia'),(6,'john','doe',1,'johndoe@example.com',101,'$2y$07$89eTTt8d7VKd3TCnvBLt4utX3CESra7Bv4PapTFgd7Oak0vzLcxD6','jdoe'),(7,'Consultant','Charles',2,'charles@example.com',200,'$2y$07$5qf7DNJJhd7f3oBHyG8dweSLjoKybHMP3/RVjdODCO7J.KTIeAkIe','charles'),(8,'Manager','Moe',3,'charles@example.com',200,'$2y$07$JZhXg8EDNA8Q7PkA3toMmuDnOt61q62ultOOq2gUZ8DrR.aK3pG7y','moe'),(10,'sdfsdfdsf','sdfdsfds',1,'sdfdsfsf',0,'$2y$07$Y0B8sOrTYD8NWIajmNdK4ed35B/yXqHDvRKp.Gn9Ft0WIMyxgHvOO','sdfdsffs'),(11,'sfdsfs','dfdsfsdf',1,'sdfsdfdsfs',0,'$2y$07$JBaRfbrnTWHQJR3LRJB9xueuPzO2HE14jxSlNq0noUC8SUdPVDHYm','dsfdsfdsf');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -169,4 +172,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-05-06 21:07:23
+-- Dump completed on 2015-05-21 10:45:37
