60f2e97599a4388c7c89d696704f3a68a001e2be
nicno90 <nicno90@hotmail.com>
2015-06-01 12:51:47 -0700

    Update list.php

--- tickets/list.php
+++ tickets/list.php
@@ -62,15 +62,15 @@
                         <?php echo createTable($user->getManagerTickets(), true, true, false);?>
                     </li>
                 <?php endif;?>
-                <li>
-                    <input type="radio" name="tabs" id="createTab">
-                    <label for="createTicket">Create Ticket</label>
-                    <form action="create.php" method="post"> 
-                        <input name="title" type ="text" placeholder="Enter Title"></input>
-                        <textarea name="description" placeholder="Enter Description"></textarea>
-                        <button class="submit" type="submit" >Submit</button>
-                    </form>
-                </li>
+                    <li>
+                        <input type="radio" name="tabs" id="createTab">
+                        <label for="createTicket">Create Ticket</label>
+                        <form action="create.php" method="post"> 
+                            <input name="title" type ="text" placeholder="Enter Title"></input>
+                            <textarea name="description" placeholder="Enter Description"></textarea>
+                            <button class="submit" type="submit" >Submit</button>
+                        </form>
+                    </li>
         </ul>
     </body>
 </html>
